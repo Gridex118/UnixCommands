2f292f00039ccd77546df61dc484b5873c38cf0b
Alex Rosegrid <rosegrid58@gmail.com>
2024-02-18 22:35:10 +0530

    Complete a more or less working implementaion

--- src/wc/wc.c
+++ src/wc/wc.c
@@ -30,6 +30,15 @@ typedef struct {
     ERROR error;
 } WCArgs;
 
+typedef struct {
+    size_t words, lines, chars, bytes;
+} Counter;
+
+static inline int is_whitespace(char c) {
+    if (c == ' ' || c == '\n' || c == '\t' || c == '\r') return 1;
+    return 0;
+}
+
 WCArgs* init_wcargs() {
     WCArgs *new_wcargs_p = malloc(sizeof(WCArgs));
     new_wcargs_p->files = malloc(sizeof(char[FILE_COUNT_MAX][ARG_LEN_MAX]));
@@ -94,8 +103,67 @@ void parse_options(char **options, int opt_count, WCArgs *wcargs_p) {
     }
     // If no flags have been toggled, display all information
     if (wcargs_p->flags_count == 0) {
-        toggle_flags("-wlmc", wcargs_p);
+        toggle_flags("-wlm", wcargs_p);
     }
+}
+
+/*
+ * Store the queried count information for file in count_str
+ */
+void count(FILE *file, char *count_str, COUNTER_FLAGS counter_flags) {
+    count_str[0] = '\0';
+    Counter counter = {0, 0, 0, 0};
+    char c;
+    while ((c = fgetc(file)) != EOF) {
+        ++counter.chars;
+        ++counter.bytes;    // Yeah, no clue how to count bytes
+        if (c == ' ' || c == '\n') {
+            while (is_whitespace(c)) {
+                if (c == '\n') ++counter.lines;
+                ++counter.chars;
+                c = fgetc(file);
+            }
+            ++counter.words;
+        }
+    }
+    if (counter_flags.lines) {
+        char lines_str[16];
+        snprintf(lines_str, 16, "%ld ", counter.lines);
+        strcat(count_str, lines_str);
+    }
+    if (counter_flags.words) {
+        char words_str[16];
+        snprintf(words_str, 16, "%ld ", counter.words);
+        strcat(count_str, words_str);
+    }
+    if (counter_flags.chars) {
+        char chars_str[16];
+        snprintf(chars_str, 16, "%ld ", counter.chars);
+        strcat(count_str, chars_str);
+    }
+    if (counter_flags.bytes) {
+        char bytes_str[16];
+        snprintf(bytes_str, 16, "%ld ", counter.bytes);
+        strcat(count_str, bytes_str);
+    }
+}
+
+int wc(WCArgs *wcargs_p) {
+    if (wcargs_p->input_type == INP_FILE) {
+        for (int i = 0; i < wcargs_p->file_count; ++i) {
+            char *file_name = (wcargs_p->files)[i];
+            FILE *file = fopen(file_name, "r");
+            if (file == NULL) return -1;
+            char count_str[COUNT_STR_LEN_MAX];
+            count(file, count_str, wcargs_p->counter_flags);
+            printf("%s %s\n", count_str, file_name);
+        }
+    } else {
+        char count_str[COUNT_STR_LEN_MAX];
+        count(stdin, count_str, wcargs_p->counter_flags);
+        printf("%s\n", count_str);
+    }
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
@@ -105,10 +173,6 @@ int main(int argc, char *argv[]) {
     if (argc > 1) {
         parse_options(argv, argc, wcargs_p);
         if (wcargs_p->error != NONE) return -1;
-        for (int i = 0; i < wcargs_p->file_count; ++i) {
-            printf("%s\n", (wcargs_p->files)[i]);
-        }
-    } else {
     }
-    return 0;
+    return wc(wcargs_p);
 }
